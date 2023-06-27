0c8c1cebfd97d2ffded70241c74c9f81e19f5564
hamiezann <136826504+hamiezann@users.noreply.github.com>
2023-06-28 07:26:28 +0800

    quotation part
    still incomplete

--- gorilla.js
+++ gorilla.js
@@ -8,7 +8,7 @@ function myFunction() {
       }
       
       // Close the dropdown if the user clicks outside of it
-      window.onclick = function(e) {
+      window.onclick = function cancel(e) {
         if (!e.target.matches('.dropbtn')) {
         var myDropdown = document.getElementById("myDropdown");
           if (myDropdown.classList.contains('show')) {
@@ -16,3 +16,51 @@ function myFunction() {
           }
         }
       }
+// Services hide and show quotation
+let a = Boolean;
+function show_hide()
+{
+if(a==1)
+{
+document.getElementById("quotation_space").style.display="inline";
+return a=0;
+}
+else 
+{
+document.getElementById("quotation_space").style.display="none";
+return a=1;
+}
+}
+
+function popupSection(){
+  // Get the modal
+let popup = document.getElementById("quotation-popup");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close-popup")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  popup.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  popup.style.display = "none";
+}
+
+}
+
+// checkbox service
+function service_select() {
+  var checkBox = document.getElementById("service");
+  var text = document.getElementById("text");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
