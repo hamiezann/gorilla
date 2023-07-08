a8a27eb377a79daaa012ed5e0835d64a5a18df13
HaziqAimanFauzi <136706089+HaziqAimanFauzi@users.noreply.github.com>
2023-07-09 01:35:01 +0800

    service h2 and service button

--- gorilla.js
+++ gorilla.js
@@ -114,6 +114,26 @@ function service_select(event){
                           
   }
 
+  // Services Button Popup
+  // const open = document.getElementById('open');
+  // const modal_container = document.getElementById('modal_container');
+  // const close = document.getElementById('close');
+  
+  // open.addEventListener('click', () => {
+  //   modal_container.classList.add('show');
+  // });
+  
+  // close.addEventListener('click', () => {
+  //   modal_container.classList.remove('show');
+  // });
+
+  function openPopup() {
+    document.getElementById('popup1').style.display = 'flex';
+  }
+
+  function closePopup() {
+    document.getElementById('popup1').style.display = 'none';
+  }
 
 
 
