49d2a8a8139ee325b695878e98ba911d108a2523
Muhammad Salehuddin <109282676+salehuddn@users.noreply.github.com>
2023-07-13 06:25:04 +0800

    calculator invoice update

--- gorilla.js
+++ gorilla.js
@@ -63,6 +63,8 @@ function service_select(event) {
 const form = document.getElementById('serviceForm');
 const checkboxes = form.querySelectorAll('input[type="checkbox"]');
 const summaryButton = document.getElementById('summaryButton');
+const carType = document.getElementById('carType').value;
+const discountCode = document.getElementById('discountCode').value; // Get the discount code input
 
 // Listen for click event on the summary button
 summaryButton.addEventListener('click', openTabWithSummary);
@@ -77,6 +79,10 @@ function openTabWithSummary() {
   const totalCost = checkedCheckboxes.reduce((total, checkbox) => {
     return total + parseInt(checkbox.value);
   }, 0);
+
+  // Apply discount if a valid discount code is entered
+  const discountAmount = getDiscountAmount(discountCode);
+  const discountedTotalCost = totalCost * (1-discountAmount);
 
   // Create an array of selected services
   const selectedServices = checkedCheckboxes.map((checkbox) => {
@@ -95,31 +101,76 @@ function openTabWithSummary() {
   // Create the content for the new tab
   const content = `<h2>Checked Services:</h2><hr><ul>${selectedServices.map((service) => `<li>${service}</li>`).join('')}</ul>`;
   const newTab = window.open();
+
   newTab.document.write(`
-    
+  <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+    <div style="text-align: center;">
+      <img src="${imageUrl}" alt="Predefined Image" width="300" height="200">
+      <h1>Service Quotation</h1>
+      <p>~ Please be mindful that the price displayed here is just an estimate and may vary based on the actual condition of the customer's car and our experts' recommendations. ~</p>
+      <hr>
+      <h2>Customer Details:</h2>
+      <table style="margin: 0 auto; border-collapse: collapse; border: 1px solid black;">
+        <tr>
+          <td style="border: 1px solid black; padding: 5px;">Name</td>
+          <td style="border: 1px solid black; padding: 5px;">${fullname}</td>
+        </tr>
+        <tr>
+          <td style="border: 1px solid black; padding: 5px;">Car Type</td>
+          <td style="border: 1px solid black; padding: 5px;">${carType}</td>
+        </tr>
+        <tr>
+          <td style="border: 1px solid black; padding: 5px;">E-mail</td>
+          <td style="border: 1px solid black; padding: 5px;">${email}</td>
+        </tr>
+        <tr>
+          <td style="border: 1px solid black; padding: 5px;">Phone number</td>
+          <td style="border: 1px solid black; padding: 5px;">${phoneNumber}</td>
+        </tr>
+        <tr>
+          <td style="border: 1px solid black; padding: 5px;">Contact Method</td>
+          <td style="border: 1px solid black; padding: 5px;">${contactMethod}</td>
+        </tr>
+      </table>
+      <hr>
+      <h2>Checked Services:</h2>
+      <table style="margin: 0 auto; border-collapse: collapse; border: 1px solid black;">
+        <tr>
+          <th style="border: 1px solid black; padding: 5px;">Service Type</th>
+          <th style="border: 1px solid black; padding: 5px;">Value</th>
+        </tr>
+        ${selectedServices.map((service) => {
+          const [type, value] = service.split(' - ');
+          return `
+            <tr>
+              <td style="border: 1px solid black; padding: 5px;">${type}</td>
+              <td style="border: 1px solid black; padding: 5px;">${value}</td>
+            </tr>
+          `;
+        }).join('')}
+      </table>
+      <hr>
+      <h1>Service Summary</h1>
+      <p>Total Cost: RM${totalCost}</p>
+      ${discountAmount > 0 ? `<p>Discount: ${discountAmount * 100}%</p>` : ''}
+          <p>Discounted Total: RM${discountedTotalCost}</p>
+    </div>
+  </div>
+`);
 
-    <img src="${imageUrl}" alt="Predefined Image" width="300" height="200">
-    <h1>Service Quotation</h1>
-    <p> ~ Please be mindful the price displayed here is just a cost overhead for the service and there may be changes to the price based on the real condition of customer cars and suggestion of our experts. ~</p>
-    <hr>
-    <hr>
-    <h2>Customer Details:</h2>
-    <hr>
-    <p>Name: ${fullname}</p>
-    <p>E-mail: ${email}</p>
-    <p>Phone number: ${phoneNumber}</p>
-    <p>Contact Method: ${contactMethod}</p>
-    <p>Service Charge: RM${totalCost}</p>
-    <br>
-    <hr>
-    <p>${content}</p>
-    <br>
-    <br><br><br><br>
+}
 
-    <h1>Service Summary</h1>
-    <p>Total Cost: RM${totalCost}</p>
-   
-  `);
+function getDiscountAmount(discountCode) {
+  // Define your discount code logic here
+  // Return the discount amount based on the entered discount code
+  // If the discount code is not valid or not applicable, return 0
+  
+  // Example:
+  if (discountCode === 'SUMMER2023') {
+    return 0.4; // Apply a discount of RM50
+  } else {
+    return 0; // No discount
+  }
 };
  
 const popupButton = document.getElementById("summaryButton");
